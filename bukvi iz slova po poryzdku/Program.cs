cd117285ad4bab3d41868173b421c79d9105945c
Misha <Misha@DESKTOP-V7OFHU5>
2023-10-17 20:35:57 +0300

    Добавьте файлы проекта.

---
+++ bukvi iz slova po poryzdku/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bukvi_iz_slova_po_poryzdku
+{
+    internal class Program
+    {
+        public static char []  Alfa(string txt)
+        {
+
+            return rez;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
